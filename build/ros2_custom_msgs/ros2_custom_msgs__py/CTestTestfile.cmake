eae059755d1e7fb221f06f7979322f80b9e28740
Goli Jyothish <jyothishgoli@gmail.com>
2026-02-19 10:54:36 +0530

    Completed custom messages assignment

---
+++ build/ros2_custom_msgs/ros2_custom_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jyothish/Desktop/ros2-custom-messages-GoliJyothish/build/ros2_custom_msgs/ros2_custom_msgs__py
+# Build directory: /home/jyothish/Desktop/ros2-custom-messages-GoliJyothish/build/ros2_custom_msgs/ros2_custom_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
